232f6820eaaa285f504cc51b2a369369470caf62
ToniIAPro73 <supertoniia@gmail.com>
2026-01-02 10:57:13 +0100

    fix: update localePrefix to 'always' for consistent routing behavior

--- i18n/navigation.ts
+++ i18n/navigation.ts
@@ -4,7 +4,7 @@ import {defineRouting} from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['es', 'en', 'de'],
   defaultLocale: 'es',
-  localePrefix: 'as-needed'
+  localePrefix: 'always'
 });
 
 export const {Link, redirect, usePathname, useRouter, getPathname} =
